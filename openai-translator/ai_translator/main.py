04fc5a7e08cd2f3d45e2cb73acdb7b959286f4a4
8000tank <8000tank@gmail.com>
2024-09-05 10:17:15 +0800

    modify main related for self

--- openai-translator/ai_translator/main.py
+++ openai-translator/ai_translator/main.py
@@ -1,26 +1,26 @@
+from translator import PDFTranslator
+from model import GLMModel, OpenAIModel
+from utils import ArgumentParser, ConfigLoader, LOG
 import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from utils import ArgumentParser, ConfigLoader, LOG
-from model import GLMModel, OpenAIModel
-from translator import PDFTranslator
 
 if __name__ == "__main__":
+    # by wy：python ai_translator/main.py --model_type OpenAIModel --openai_model gpt-4o-mini --file_format markdown --book tests/test.pdf
     argument_parser = ArgumentParser()
     args = argument_parser.parse_arguments()
     config_loader = ConfigLoader(args.config)
 
     config = config_loader.load_config()
 
-    model_name = args.openai_model if args.openai_model else config['OpenAIModel']['model']
-    api_key = args.openai_api_key if args.openai_api_key else config['OpenAIModel']['api_key']
+    model_name = args.openai_model or config['OpenAIModel']['model']
+    api_key = args.openai_api_key or config['OpenAIModel']['api_key']
     model = OpenAIModel(model=model_name, api_key=api_key)
 
-
-    pdf_file_path = args.book if args.book else config['common']['book']
-    file_format = args.file_format if args.file_format else config['common']['file_format']
+    pdf_file_path = args.book or config['common']['book']
+    file_format = args.file_format or config['common']['file_format']
 
     # 实例化 PDFTranslator 类，并调用 translate_pdf() 方法
     translator = PDFTranslator(model)
--- openai-translator/ai_translator/model/openai_model.py
+++ openai-translator/ai_translator/model/openai_model.py
@@ -8,6 +8,7 @@ from model import Model
 from utils import LOG
 from openai import OpenAI
 
+
 class OpenAIModel(Model):
     def __init__(self, model: str, api_key: str):
         self.model = model
@@ -17,34 +18,26 @@ class OpenAIModel(Model):
         attempts = 0
         while attempts < 3:
             try:
-                if self.model == "gpt-3.5-turbo":
-                    response = self.client.chat.completions.create(
-                        model=self.model,
-                        messages=[
-                            {"role": "user", "content": prompt}
-                        ]
-                    )
-                    translation = response.choices[0].message.content.strip()
-                else:
-                    response = self.client.completions.create(
-                        model=self.model,
-                        prompt=prompt,
-                        max_tokens=150,
-                        temperature=0
-                    )
-                    translation = response.choices[0].text.strip()
-
+                response = self.client.chat.completions.create(
+                    model=self.model,
+                    messages=[
+                        {"role": "user", "content": prompt}
+                    ],
+                    max_tokens=150,
+                    temperature=0
+                )
+                translation = response.choices[0].message.content.strip()
                 return translation, True
             except openai.RateLimitError as e:
                 attempts += 1
-                if attempts < 3:
-                    LOG.warning("Rate limit reached. Waiting for 60 seconds before retrying.")
-                    time.sleep(60)
-                else:
+                if attempts >= 3:
                     raise Exception("Rate limit reached. Maximum attempts exceeded.")
+                LOG.warning("Rate limit reached. Waiting for 60 seconds before retrying.")
+                time.sleep(60)
             except openai.APIConnectionError as e:
                 print("The server could not be reached")
-                print(e.__cause__)  # an underlying Exception, likely raised within httpx.            except requests.exceptions.Timeout as e:
+                # an underlying Exception, likely raised within httpx.            except requests.exceptions.Timeout as e:
+                print(e.__cause__)
             except openai.APIStatusError as e:
                 print("Another non-200-range status code was received")
                 print(e.status_code)
